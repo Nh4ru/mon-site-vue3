7e597db3fc9bf36ecf8a614b4fcb661bca5a1906
Tristan Fernandez <93312835+Nh4ru@users.noreply.github.com>
2022-06-22 19:54:16 +0200

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navbar">
+    <header :class="{'scrolled-nav' : scrollPosition }">
+      <nav>
+        <div class="branding">
+          <img src="@/assets/Vintage-Tristan-Fernandez-1000px.png" alt="">
+        </div>
+        <ul v-show="!mobile" class="navigation">
+          <li><router-link class="navigation__link"></router-link></li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
